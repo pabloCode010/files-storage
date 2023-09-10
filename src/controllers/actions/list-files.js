f1eb99e988b4482a8a09dbd8fea2a5a3dca08b48
Pablo <algorithmxplorer@gmail.com>
2023-09-10 13:48:10 -0600

    repair error path storage

--- src/controllers/actions/list-files.js
+++ src/controllers/actions/list-files.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require("path")
 
-const storagePath = path.join(__dirname, "..", "..", "storage");
+const storagePath = path.join(__dirname, "..", "..", "..", "storage");
 
 module.exports = (req, res, next) => {
     try {
